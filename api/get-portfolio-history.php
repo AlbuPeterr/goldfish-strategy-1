57b63953468cdeae7352046a71e699edfa57b52e
AlbuPeter <104072231+AlbuPeter@users.noreply.github.com>
2025-07-04 14:31:02 +0200

    Update get-portfolio-history.php

--- api/get-portfolio-history.php
+++ api/get-portfolio-history.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$conn = new mysqli("localhost", "root", "", "goldfish");
+$conn = new mysqli("localhost", "root", "", "goldfish-strategy");
 if ($conn->connect_error) {
     die("Kapcsolódási hiba: " . $conn->connect_error);
 }
